6275ad107e75d57f1bf7c61b295463c4c917cd59
pslcorrea <pslcorrea@gmail.com>
2022-10-16 12:06:57 -0300

    build V1.001.1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="" id="" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Pedidos">Pedidos</router-link>
+  </div>
+
+</template>
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>>
