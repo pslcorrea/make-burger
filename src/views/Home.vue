9f67939b92e6509d0af816d55e673f95f65d8048
pslcorrea <pslcorrea@gmail.com>
2022-10-16 13:12:59 -0300

    configurado banner da aplicação, acrescentando iagem de fundo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="main-container">
-    <h1>Home</h1>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu burguer:</h1>
+    </div>
+
   </div>
 </template>
 
 <script>
-
+import Banner from '../components/Banner.vue'
 
 export default {
-
+  name: 'Home',
+  components: {
+    Banner,
+  }
 }
 
 </script>
